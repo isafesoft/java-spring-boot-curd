c87cdf6d4a339e5dcafce80601c7567300f4041d
Mark Xu <isafesoft@hotmail.com>
2018-12-21 10:15:08 -0500

    add service

---
+++ src/main/java/com/mk/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.mk.service;
+
+import com.mk.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    public List<Student> findAll();
+
+    public Student findById(int id);
+
+    public void save(Student student);
+
+    public void deleteById(int id);
+}
---
+++ src/main/java/com/mk/service/StudentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.mk.service;
+
+import com.mk.dao.StudentDAO;
+import com.mk.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    private StudentDAO studentDAO;
+    @Autowired
+    public StudentServiceImpl(StudentDAO studentDAO) {
+        this.studentDAO = studentDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Student findById(int id) {
+        return studentDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        studentDAO.save(student);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        studentDAO.deleteById(id);
+    }
+}
